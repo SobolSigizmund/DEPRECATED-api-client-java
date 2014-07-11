777e72a4373b15042d55719010b6d3238da85e86
cassiedoll <cassandradoll@gmail.com>
2014-07-11 09:38:02 -0700

    Add fetching datasets by projectId

--- src/main/java/com/google/cloud/genomics/api/client/commands/ListDatasetsCommand.java
+++ src/main/java/com/google/cloud/genomics/api/client/commands/ListDatasetsCommand.java
@@ -22,15 +22,27 @@ import com.google.api.services.genomics.Genomics;
 import java.io.IOException;
 import java.util.Map;
 
-@Parameters(commandDescription = "List past datasets used by this command line")
+@Parameters(commandDescription = "List recent datasets used by this command line, or list them " +
+    "by project number")
 public class ListDatasetsCommand extends BaseCommand {
 
   @Parameter(names = "--details",
-      description = "Whether to look up each Dataset in the API to get its full details.")
+      description = "Whether to look up each recent datasets in the API to get their full details.")
   public boolean includeDetails = false;
+
+  @Parameter(names = {"--project_number", "--project_id"},
+      description = "Get datasets for a specific Google Developer's Console project number " +
+          "(By default only recently used datasets will be shown)")
+  public Long projectId;
 
   @Override
   public void handleRequest(Genomics genomics) throws IOException {
+
+    if (projectId != null) {
+      Genomics.Datasets.List request = genomics.datasets().list().setProjectId(projectId);
+      System.out.println(request.execute().toPrettyString());
+      return;
+    }
 
     Map<String, String> datasets = getPreviousDatasets();
     for (Map.Entry<String, String> dataset : datasets.entrySet()) {
