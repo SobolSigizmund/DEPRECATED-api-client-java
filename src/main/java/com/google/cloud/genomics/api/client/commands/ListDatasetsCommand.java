7d10b2d0329e6cd7dc7427986e9ea259dcde04ec
cassiedoll <cassandradoll@gmail.com>
2014-07-11 11:39:19 -0700

    listdatasets now looks up variant summaries

--- src/main/java/com/google/cloud/genomics/api/client/commands/ListDatasetsCommand.java
+++ src/main/java/com/google/cloud/genomics/api/client/commands/ListDatasetsCommand.java
@@ -17,7 +17,11 @@ package com.google.cloud.genomics.api.client.commands;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.genomics.Genomics;
+import com.google.api.services.genomics.model.Dataset;
+import com.google.api.services.genomics.model.GetVariantsSummaryResponse;
+import com.google.api.services.genomics.model.ListDatasetsResponse;
 
 import java.io.IOException;
 import java.util.Map;
@@ -27,7 +31,8 @@ import java.util.Map;
 public class ListDatasetsCommand extends BaseCommand {
 
   @Parameter(names = "--details",
-      description = "Whether to look up each recent datasets in the API to get their full details.")
+      description = "Whether to display the full details of each dataset " +
+          "(besides just name and ID)")
   public boolean includeDetails = false;
 
   @Parameter(names = {"--project_number", "--project_id"},
@@ -39,24 +44,53 @@ public class ListDatasetsCommand extends BaseCommand {
   public void handleRequest(Genomics genomics) throws IOException {
 
     if (projectId != null) {
-      Genomics.Datasets.List request = genomics.datasets().list().setProjectId(projectId);
-      System.out.println(request.execute().toPrettyString());
+      ListDatasetsResponse datasets = genomics.datasets().list().setProjectId(projectId).execute();
+      for (Dataset dataset : datasets.getDatasets()) {
+        printDataset(genomics, dataset.getId(), dataset.getName(), dataset);
+      }
+
+    } else {
+      Map<String, String> datasets = getPreviousDatasets();
+      if (datasets.isEmpty()) {
+        System.out.println("There aren't any recently used datasets. " +
+            "Specify a --project_id flag to get datasets for a specific project, or use the " +
+            "'createdataset' command to make a new dataset.");
+        return;
+      }
+
+      for (Map.Entry<String, String> dataset : datasets.entrySet()) {
+        printDataset(genomics, dataset.getKey(), dataset.getValue(), null);
+      }
+    }
+  }
+
+  private void printDataset(Genomics genomics, String id, String name, Dataset dataset)
+      throws IOException {
+    System.out.println(id + ": " + name);
+    if (!includeDetails) {
       return;
     }
 
-    Map<String, String> datasets = getPreviousDatasets();
-    if (datasets.isEmpty()) {
-      System.out.println("There aren't any recently used datasets. " +
-          "Specify a --project_id flag to get datasets for a specific project, or use the " +
-          "'createdataset' command to make a new dataset.");
-      return;
+    if (dataset == null) {
+      dataset = getDataset(genomics, id);
+    }
+    if (dataset != null) {
+      System.out.println(dataset.toPrettyString() + "\n");
     }
 
-    for (Map.Entry<String, String> dataset : datasets.entrySet()) {
-      System.out.println(dataset.getKey() + ": " + dataset.getValue());
-
-      if (includeDetails) {
-        System.out.println(getDataset(genomics, dataset.getKey()).toPrettyString() + "\n");
+    try {
+      GetVariantsSummaryResponse summary = genomics.variants().getSummary()
+          .setDatasetId(id).execute();
+      if (summary != null && summary.getContigBounds() != null) {
+        // Only print out a variant summary if one exists
+        System.out.println("variant summary: " + summary.toPrettyString() + "\n");
+      }
+    } catch (GoogleJsonResponseException e) {
+      if (e.getDetails().getCode() != 403) {
+        // The variants APIs aren't fully public, so 403s are common
+        // Only display errors which aren't an authorization fail
+        // TODO: Remove this if block once the variant APIs are widely available
+        throw e;
       }
     }
   }
